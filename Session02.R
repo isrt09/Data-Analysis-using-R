fb596e3868429fde217b0268abced6df01f499ba
isrt09 <mazedur@gmail.com>
2020-10-12 02:46:08 +0600

    R as Object or Vector

--- Session02.R
+++ Session02.R
@@ -21,3 +21,56 @@ x
 typeof(x)
 
 is.integer(x)
+
+# R Objects ( Vectors)
+
+x <- c(1,2,3,4,5,6,7,8,9,10)
+x
+
+odd_number <- x[x%%2!=0]
+odd_number
+
+even_number <- x[x%%2==0]
+even_number
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
