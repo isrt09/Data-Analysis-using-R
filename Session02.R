10959101cd1701449cd08bcba510d52cca33dbe7
isrt09 <mazedur@gmail.com>
2020-10-12 02:05:21 +0600

    Data Types in R

---
+++ Session02.R
@@ -0,0 +1,23 @@
+# R Data Types ....
+
+x <- 35.2
+x
+typeof(x)
+
+x <- 30
+x
+typeof(x)
+
+y <- as.integer(x)
+y
+typeof(y)
+
+x <- "Mazedur Rahman"
+x
+typeof(x)
+
+x <- TRUE
+x
+typeof(x)
+
+is.integer(x)
