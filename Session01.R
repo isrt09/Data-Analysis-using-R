585eed1f112b6726ff4c757a1354015f40b1d69c
isrt09 <mazedur@gmail.com>
2020-10-11 20:36:12 +0600

    R as Data Generation

--- Session01.R
+++ Session01.R
@@ -21,3 +21,72 @@ ls()
 
 rm(x,y)
 ls()
+
+# R as Data Generation
+
+x<- 1:20
+x
+
+y <- seq(1,20)
+y
+
+y <- seq(1,30,2)
+y
+
+y <- seq(1,20,5)
+y
+
+z <- seq(-4,4,.05)
+z
+
+a <- rep("a",10)
+a
+
+sequence(c(3,4,5,6))
+
+gl(2,5,label=c('FRIDAY','SATURDAY'))
+
+gl(3,4,length = 20)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
